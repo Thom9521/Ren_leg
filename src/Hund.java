14a9488af7d1d316ffc9a548724a54353fbca73a
Thomas Christensen <31951107+Thom9521@users.noreply.github.com>
2017-09-29 13:26:49 +0200

    Initial commit

---
+++ src/Hund.java
@@ -0,0 +1,44 @@
+public class Hund {
+
+
+
+    String navn = "";
+    int alder = 0;
+    String race = "";
+
+
+    Hund (String hundensNavn){
+        navn = hundensNavn;
+        System.out.println("Hundens navn er " + navn);
+    }
+
+    public void setAlderDreng(int hundensAlderDreng){
+        alder = hundensAlderDreng;
+        System.out.println("Og han er " + hundensAlderDreng + " år gammel");
+    }
+
+    public void setAlderPige (int hundensAlderPige){
+        alder = hundensAlderPige;
+        System.out.println("Og hun er " + hundensAlderPige + " år gammel");
+    }
+
+    public void setRace (String hundensRace) {
+        race = hundensRace;
+        System.out.println(navn + " er en " + hundensRace);
+
+    }
+    public void hop(){
+        System.out.println(navn + " hopper");
+    }
+
+    public void spis(){
+        System.out.println(navn + " spiser hundemad");
+    }
+
+    public void leg() {
+        System.out.println(navn + " leger med sit hundelegetøj");
+    }
+
+}
+
+
---
+++ src/Hundehjemmet_main.java
@@ -0,0 +1,26 @@
+public class Hundehjemmet_main {
+    public static void main(String[] args) {
+
+        Hund hund1;
+        hund1 = new Hund ("Laban");
+        hund1.setAlderDreng(1);
+        hund1.setRace("Yorkshire Terrier");
+        hund1.hop();
+
+        System.out.println(" ");
+
+        Hund hund2;
+        hund2 = new Hund ("Mille");
+        hund2.setAlderPige(12);
+        hund2.setRace("West Highland White Terrier");
+        hund2.spis();
+
+        System.out.println(" ");
+
+        Hund hund3;
+        hund3 = new Hund("Daisy");
+        hund3.setAlderPige(4);
+        hund3.setRace("Labrador");
+        hund3.leg();
+    }
+}
